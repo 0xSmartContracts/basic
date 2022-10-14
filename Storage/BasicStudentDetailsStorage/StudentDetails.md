5be87215b414a7e60924453fb578c09a6eda0b8f
Voyager <115632257+0xVoyager@users.noreply.github.com>
2022-10-14 14:44:48 +0530

    Basic Student Details Storage

---
+++ Storage/BasicStudentDetailsStorage/StudentDetails.md
@@ -0,0 +1,27 @@
+# User Details Sotrage
+
+
+### Solidity Version:
+* 0.8.17
+
+
+### Parameters:
+* walletAddress;
+* registerNumber; 
+* firstName;
+* lastName;
+* schoolName;
+* grade;
+* emailAddress;
+* bloodGroup;
+* age;
+
+### Storage Array:
+* Student [ ]
+
+### Call Data:
+* student - Datatype uint256 (id)
+
+### Polygon Testnet Deployment
+* Contract Address - 0xF30C276C1fA2602192B8AaDc508967ffb9b6F463
+* TestData TxDetail - 0x4b1218ab6d066f3df909f4e3fa9ec48d4228c4f5bd070b2da30cb6354f8dca59
---
+++ Storage/BasicStudentDetailsStorage/StudentDetails.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+contract StudentDetails{
+
+    Student[] public student;
+
+    struct Student {
+        address walletAddress;
+        uint256 registerNumber; 
+        string firstName;
+        string lastName;
+        string schoolName;
+        string grade;
+        string emailAddress;
+        string bloodGroup;
+        uint256 age;
+    }
+
+
+    function addStudent(address _walletAddress, uint256 _registerNumber, string memory _firstName, string memory _lastName, string memory _schoolName, string memory _grade, string memory _emailAddress, string memory _bloodGroup,uint256 _age) public{
+        student.push(Student(_walletAddress,_registerNumber,_firstName,_lastName,_schoolName,_grade,_emailAddress,_bloodGroup,_age));
+    }
+    
+}
