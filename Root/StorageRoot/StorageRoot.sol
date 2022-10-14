028fbb12b08d8b7df3d00cd1a7e9413cce3d26ee
Voyager <115632257+0xVoyager@users.noreply.github.com>
2022-10-14 14:49:19 +0530

    Student Details Contract Added

--- Root/StorageRoot/StorageRoot.sol
+++ Root/StorageRoot/StorageRoot.sol
@@ -3,11 +3,23 @@ pragma solidity 0.8.17;
 
 
 import "https://github.com/0xSmartContracts/basic/blob/main/Storage/BasicUserDetailsStorage/UserDetails.sol";
+import "https://github.com/0xSmartContracts/basic/blob/main/Storage/BasicStudentDetailsStorage/StudentDetails.sol";
 
 contract StorageRoot {
     UserDetails[] public userDetailsArray;
+    StudentDetails[] public studentDetailsArray;
+    
+    
     function createUserDetailsContract() public {
        UserDetails userDetails = new UserDetails();
        userDetailsArray.push(userDetails);
     }
+    
+    
+    function createStudentDetailsContract() public{
+        StudentDetails studentDetails = new StudentDetails();
+        studentDetailsArray.push(studentDetails);
+    }
+    
+    
 }
